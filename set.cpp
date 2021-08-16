27a114a506ef8d189770e36fa4f4be6949f0fca6
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-08-16 09:16:03 +0530

    unordered_map

--- set.cpp
+++ set.cpp
@@ -4,11 +4,12 @@
 // Stores element in sorted order
 
 #include <iostream>
-#include<set>
+#include <set>
 
 using namespace std;
 
-int main(){
+int main()
+{
     // declaration
     set<int> s;
 
@@ -20,24 +21,24 @@ int main(){
     s.insert(5);
 
     // printing the set
-    set<int, greater<int> >::iterator itr;
+    set<int, greater<int>>::iterator itr;
     for (itr = s.begin(); itr != s.end(); itr++)
     {
-        cout <<*itr<<" ";
+        cout << *itr << " ";
     }
-    cout<<endl;
+    cout << endl;
 
     // finding any elements require O(log n)
     // "find" function returns the iterator in O(log n)
-    cout<<"Print 5 if present ";
-    cout<<*s.find(5)<<endl;
+    cout << "Print 5 if present ";
+    cout << *s.find(5) << endl;
     // "count" function returns bool value in O(log n) time
-    cout<<"Is 9 present in the set? "<<s.count(9)<<endl;
+    cout << "Is 9 present in the set? " << s.count(9) << endl;
 
     // deleting all elements in the set
-    cout<<"Is the set empty? "<<s.empty()<<endl;
+    cout << "Is the set empty? " << s.empty() << endl;
     s.clear();
-    cout<<"Is the set empty now? "<<s.empty()<<endl;
+    cout << "Is the set empty now? " << s.empty() << endl;
 
     return 0;
 }
